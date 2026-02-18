fb74ba9c0673c0d36a649fe4297b359fc04a36c4
Saloni paney-ux <workspaceworkout0716@gmail.com>
2026-02-18 16:26:36 +0530

    pattern

--- JAVabasis/Halfpyramid.java
+++ JAVabasis/Halfpyramid.java
@@ -1,15 +1,17 @@
 public class Halfpyramid {
     public static void main(String args[])
     {
-        int n=4;
-        for(int line=1;line<=n;line++) 
+        int n=9;
+        for(int line=n;line>=1;line--)
         {
-            for(int num=1;num<=line;num++)
+            for(int num=n;num>=line;num--)
             {
-                System.out.print(num);  
+                System.out.print(num);
             }
-             System.out.println();
-        }    
-            }
+            
+      System.out.println();  
+    
+    }
         
-        }
+}
+}
