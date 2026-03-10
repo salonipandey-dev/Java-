a46846d745c2cf09b86f6bb235e157ea9ae7e038
Saloni paney-ux <workspaceworkout0716@gmail.com>
2026-03-10 22:05:24 +0530

    ddd

---
+++ DSAjava/ARRAY/Pairsarray.java
@@ -0,0 +1,25 @@
+package ARRAY;
+
+public class Pairsarray {
+    public static void Printpairs(int numbers[])
+    {
+        int tp=0;
+        for(int i=0;i<numbers.length;i++)
+        {
+            int curr=numbers[i];
+            for(int j=i+1;j<numbers.length;j++)
+            {
+                System.out.print("("+curr+","+numbers[j]+")");
+                tp++;
+            }   System.out.println();
+        }
+        System.out.println("Total pairs: " + tp);
+    }
+    public static void main(String args[])
+    {
+        int number[]={2,4,6,8,10};
+        Printpairs(number); 
+
+    }
+    
+}
