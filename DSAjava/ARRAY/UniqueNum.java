fe0126867c9fdfbc1464e94af949e11c11bdea50
Saloni paney-ux <workspaceworkout0716@gmail.com>
2026-03-10 12:19:40 +0530

    aa

---
+++ DSAjava/ARRAY/UniqueNum.java
@@ -0,0 +1,18 @@
+package ARRAY;
+
+public class UniqueNum {
+    public static int Unique(int numbers[])
+    {
+        int uniquee=0;
+        for(int i=0;i<numbers.length;i++)
+        {
+            uniquee=uniquee^numbers[i];
+        }
+        return uniquee;
+    }
+    public static void main(String args[])
+    {
+        int numbers[]={1,1,3,2,2,4,4};
+        System.out.println(Unique(numbers));
+    }
+}
