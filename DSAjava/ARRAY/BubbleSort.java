9724a6fb848c4977e76c847b7e90045c409a4886
Saloni paney-ux <workspaceworkout0716@gmail.com>
2026-04-07 12:21:36 +0530

    update

---
+++ DSAjava/ARRAY/BubbleSort.java
@@ -0,0 +1,30 @@
+package ARRAY;
+
+public class BubbleSort {
+    public static void BubbleSOrt(int arr[])
+    {
+        for(int i=0;i<arr.length-1;i++)
+        {
+            for(int j=0;j<arr.length-1-i;j++)
+            {
+                if(arr[j]>arr[j+1])
+                {
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+
+                }
+            }
+        }
+    }
+    public static void main(String args[])
+    {
+        int arr[]={1,0,1,0,1,0};
+        BubbleSOrt(arr);
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+    }
+    
+}
