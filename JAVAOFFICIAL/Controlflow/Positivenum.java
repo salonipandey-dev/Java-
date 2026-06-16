377ece2a77945fa3e0af2b46eb2150941078d877
Saloni paney-ux <workspaceworkout0716@gmail.com>
2026-06-16 21:22:11 +0530

    practice

---
+++ JAVAOFFICIAL/Controlflow/Positivenum.java
@@ -0,0 +1,21 @@
+package JAVAOFFICIAL.Controlflow;
+import java.util.Scanner;
+
+public class Positivenum {
+    public static void main(String args[])
+    {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int num = sc.nextInt();
+        if(num>0)
+        {
+            System.out.println("The number is positive.");
+
+        }
+       sc.close();
+       
+    
+  }
+    
+
+}
