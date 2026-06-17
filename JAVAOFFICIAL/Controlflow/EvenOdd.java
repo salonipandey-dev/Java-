93a779520d2604c6c81acb08d575fcae5d0c890c
Saloni paney-ux <workspaceworkout0716@gmail.com>
2026-06-17 10:44:28 +0530

    practice

---
+++ JAVAOFFICIAL/Controlflow/EvenOdd.java
@@ -0,0 +1,21 @@
+package JAVAOFFICIAL.Controlflow;
+import java.util.*;
+
+public class EvenOdd {
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        if(num % 2 == 0)
+        {
+            System.out.println("The number is even.");
+        }
+        else
+        {
+            System.out.println("The number is odd.");
+        }
+        sc.close();
+
+    }
+}
---
+++ JAVAOFFICIAL/Controlflow/Voting.java
@@ -0,0 +1,16 @@
+package JAVAOFFICIAL.Controlflow;
+import java.util.*;
+
+public class Voting {
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();
+        if(age >= 18)
+        {
+            System.out.println("You are eligible to vote.");
+        }
+        sc.close();
+    }
+}
