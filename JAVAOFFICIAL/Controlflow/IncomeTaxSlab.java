fb588c119efbf9adb1279ce6c5b938dc104f475b
Saloni paney-ux <workspaceworkout0716@gmail.com>
2026-06-17 12:09:20 +0530

    practice

---
+++ JAVAOFFICIAL/Controlflow/IncomeTaxSlab.java
@@ -0,0 +1,24 @@
+package JAVAOFFICIAL.Controlflow;
+
+public class IncomeTaxSlab {
+    public static void main(String args[])
+    {
+        int income = 600000;
+        if(income <= 250000)
+        {
+            System.out.println("No tax");
+        }
+        else if(income > 250000 && income <= 500000)
+        {
+            System.out.println("5% tax");
+        }
+        else if(income > 500000 && income <= 1000000)
+        {
+            System.out.println("20% tax");
+        }
+        else
+        {
+            System.out.println("30% tax");
+        }
+    }
+}
---
+++ JAVAOFFICIAL/Controlflow/MenuDrivenATM.java
@@ -0,0 +1,49 @@
+package JAVAOFFICIAL.Controlflow;
+import java.util.Scanner;
+
+
+public class MenuDrivenATM {
+    public static void main(String args[])
+    {
+        int balance = 100000; 
+        double withdraw ;
+        System.out.println("Welcome to the ATM");
+        System.out.println("1. Check Balance");
+        System.out.println("2. Withdraw");  
+        System.out.println("3. Exit");
+        while(true)
+        {
+            System.out.print("choose an option :");
+            Scanner sc = new Scanner(System.in);
+            int choice = sc.nextInt();
+            switch(choice)
+            {
+                case 1:
+                    System.out.println("Balance : " + balance);
+                    break;
+                case 2:
+                    System.out.print("Enter money to be withdrawn:");
+                    withdraw = sc.nextDouble();
+                    if(balance >= withdraw)
+                    {
+                        balance = (int)(balance - withdraw);
+                        System.out.println("Please collect your money");
+                    }
+                    else
+                    {
+                        System.out.println("Insufficient Balance");
+                    }
+                    System.out.println("");
+                    break;
+                case 3:
+                    System.out.println("Thank you for using ATM. Goodbye!");
+                    sc.close();
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid option. Please try again.");
+                
+            }
+        }
+    }
+
+}
