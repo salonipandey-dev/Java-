8780582fab709040e71585e8d8ec5cac5736c2dd
Saloni paney-ux <workspaceworkout0716@gmail.com>
2026-06-20 16:10:39 +0530

    practice

--- JAVAOFFICIAL/LOOPS/ArmStrongnum.java
+++ JAVAOFFICIAL/LOOPS/ArmStrongnum.java
@@ -4,18 +4,29 @@ import java.util.*;
 public class ArmStrongnum {
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
-        System.out.println("enter the number bro to check :");
-        int num= sc.nextInt();
-
-        answer = IsArmstrong();
-        System.out.println(answer);
+      //  System.out.println("enter the number to check :");
+        // int num= sc.nextInt();
+        for(int i =100; i<=1000;i++)
+        {
+            if (IsArmstrong(i)) {
+                System.out.print(i + " ");
+            }
+        }
+        sc.close();
     }
 
-     static boolean IsArmstrong(){
+    static boolean IsArmstrong(int n){
+        int ori =n;
+        int sum=0;
+        while(n>0)
+        {
+            int rem =n%10;
+            n =n/10;
+            sum = sum+ rem * rem*rem;
+        }
+    
+        return sum == ori;
 
-     }
-
-
-     }
+    }
     
 }
