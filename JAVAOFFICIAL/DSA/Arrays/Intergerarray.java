9a4155f89661d5659cba02420e164fd78930d366
Saloni paney-ux <workspaceworkout0716@gmail.com>
2026-07-03 19:42:17 +0530

    practice

--- JAVAOFFICIAL/DSA/Arrays/Intergerarray.java
+++ JAVAOFFICIAL/DSA/Arrays/Intergerarray.java
@@ -1,4 +1,23 @@
 package JAVAOFFICIAL.DSA.Arrays;
+import java.util.*;
 
 public class Intergerarray {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int[] arr =new int[5];
+        System.out.print("Enter the array elements :");
+        for(int i=0;i<arr.length;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        System.out.print("Array elements are :");
+        System.out.print("[ ");
+        for(int i=0;i<arr.length;i++)
+        {
+        System.out.print(arr[i]);
+        System.out.print(" ");
+        }
+        System.out.print("]");
+        sc.close();
+    }
 }
---
+++ JAVAOFFICIAL/DSA/Arrays/Storingnum.java
@@ -0,0 +1,19 @@
+package JAVAOFFICIAL.DSA.Arrays;
+
+public class Storingnum {
+    public static void main(String[] args) {
+        int[] arr=new int[5];
+        arr[0]=5;
+        arr[1]=6;
+        arr[4]=9;
+        arr[3]=5;
+        arr[2]=20;
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.print(arr[i]);
+            System.out.print(" ");
+        }
+        
+    }
+    
+}
