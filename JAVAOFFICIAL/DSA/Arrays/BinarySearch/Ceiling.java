e31240c37df4f2f95211c347f2023ef9fa810a08
Saloni paney-ux <workspaceworkout0716@gmail.com>
2026-07-05 20:38:13 +0530

    practice

---
+++ JAVAOFFICIAL/DSA/Arrays/BinarySearch/Ceiling.java
@@ -0,0 +1,28 @@
+package JAVAOFFICIAL.DSA.Arrays.BinarySearch;
+
+public class Ceiling {
+    static int ceilingArray(int[] arr, int target)
+    {
+        int start =0;
+        int end=arr.length-1;
+        while(start<=end)
+        {
+            int mid=start+(end-start)/2;
+            if(arr[mid]<=target)
+            {
+                start=mid+1;
+            }
+            else if(arr[mid]>target)
+            {
+                end=mid-1;
+            }
+            else
+            {
+                return mid;
+            }
+
+        }
+        return start;
+    }
+    
+}
