8867c30fd5c341508a11273d391c4be0c49c677b
Saloni paney-ux <workspaceworkout0716@gmail.com>
2026-07-05 20:12:28 +0530

    practice

---
+++ JAVAOFFICIAL/DSA/Arrays/Linearsearch/LinearSearch.java
@@ -0,0 +1,22 @@
+package JAVAOFFICIAL.DSA.Arrays.Linearsearch;
+
+public class LinearSearch {
+    static int linearSearch(int[] arr, int target)
+    {
+        for(int index=0;index<arr.length;index++)
+        {
+            if(arr[index]==target)
+            {
+                return index;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] arr={99,88,77,66,55,44,33,22,11};
+        int target=44;
+        int result=linearSearch(arr,target);
+        System.out.println("Index for key found at :" + result);
+    }
+    
+}
