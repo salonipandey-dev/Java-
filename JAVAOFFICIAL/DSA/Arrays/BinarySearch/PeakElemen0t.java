3780378deb583110d67b03c186c9c39016ac1b52
Saloni paney-ux <workspaceworkout0716@gmail.com>
2026-07-06 17:01:17 +0530

    practice

---
+++ JAVAOFFICIAL/DSA/Arrays/BinarySearch/PeakElemen0t.java
@@ -0,0 +1,30 @@
+package JAVAOFFICIAL.DSA.Arrays.BinarySearch;
+
+public class PeakElemen0t {
+    public static void main(String[] args)
+    {
+        int[] arr={1,2,3,1};
+        int ans=peakElement(arr);
+        System.out.println(ans);
+    }
+    static int peakElement(int[] arr)
+    {
+        int start =0;
+        int end=arr.length-1;
+        while(start<end)
+        {
+            int mid=start+(end-start)/2;
+            if(arr[mid]>arr[mid+1])
+            {
+                end=mid;
+            }
+            else
+            {
+                start=mid+1;
+
+            }
+            
+        }
+        return start;
+    }
+}
