8f2975a38c650e7552464f50bbe5fd2a42d23923
Saloni paney-ux <workspaceworkout0716@gmail.com>
2026-07-06 12:53:43 +0530

    practice

--- JAVAOFFICIAL/DSA/Arrays/BinarySearch/findfirstandlastpoistion.java
+++ JAVAOFFICIAL/DSA/Arrays/BinarySearch/findfirstandlastpoistion.java
@@ -10,12 +10,13 @@ public class findfirstandlastpoistion {
     static int findFirst(int[] nums, int target) {
         int start= 0;
         int  end = nums.length - 1;
-        int first = -1;
+        int first=-1;
         while (start <= end) {
             int mid = start + (end - start) / 2;
 
             if (nums[mid] == target) {
                 first = mid;
+            
                 end = mid - 1; // Continue searching in the left half
             } else if (nums[mid] < target) {
                 start = mid + 1;
