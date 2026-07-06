6e26786ea7028c14674e641f2b9c5f396520f79d
Saloni paney-ux <workspaceworkout0716@gmail.com>
2026-07-06 16:45:12 +0530

    practice

--- JAVAOFFICIAL/DSA/Arrays/BinarySearch/IfiniteArray.java
+++ JAVAOFFICIAL/DSA/Arrays/BinarySearch/IfiniteArray.java
@@ -1,6 +1,28 @@
 package JAVAOFFICIAL.DSA.Arrays.BinarySearch;
 
 public class IfiniteArray {
+    public static void main(String[] args) {
+        int[] arr={3,4,7,9,10,90,100,130,140,160,170};
+        int target=10;
+        int ans=FindingRange(arr,target);
+        System.out.println(ans);
+        
+        
+    }
+    static int FindingRange(int[] arr, int target)
+    {
+        //first findthe range
+        int start =0;
+        int end=1;
+        while(target>arr[end])
+        {
+            int newstart=end+1;
+            end=end+(end-start+1)*2;
+            start=newstart;
+        }
+        return Search(arr,target,start,end);
+    }
+ 
     static int Search(int[] arr, int target,int start,int end)
     {
         while(start<=end)
@@ -22,22 +44,6 @@ public class IfiniteArray {
         return -1;
 
     }
-    static int FindingRange(int[] arr, int target)
-    {
-        //first findthe range
-        int start =0;
-        int end=1;
-        while(target>arr[end])
-        {
-            int newstart=end+1;
-            end=end+(end-start+1)*2;
-            start=newstart;
-        }
-        return -1;
-    }
-    public static void main(String[] args) {
-        
-        
-    }
+    
     
 }
