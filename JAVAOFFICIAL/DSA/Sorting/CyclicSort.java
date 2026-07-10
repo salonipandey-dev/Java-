b30ba5a0af1ebecd8879b647ccf6d104aff614b5
Saloni paney-ux <workspaceworkout0716@gmail.com>
2026-07-10 19:29:09 +0530

    practice

--- JAVAOFFICIAL/DSA/Sorting/CyclicSort.java
+++ JAVAOFFICIAL/DSA/Sorting/CyclicSort.java
@@ -1,3 +1,31 @@
+package JAVAOFFICIAL.DSA.Sorting;
 public class CyclicSort {
+    public static void main(String[] args) {
+        int[] arr={3,5,2,1,4};
+        Cyclicsort(arr);
+        System.out.println("Sorted array is :");
     
+}
+        static void Cyclicsort(int[] arr)
+    {
+        int i=0;
+        while(i<arr.length)
+        {
+            int correctindex=arr[i]-1;
+            if(arr[i]!=arr[correctindex])
+            {
+                swap(arr,i,correctindex);
+            }
+            else
+            {
+                i++;
+            }
+        }
+    }
+    static void swap(int[] arr,int first,int second)
+    {
+        int temp=arr[first];
+        arr[first]=arr[second];
+        arr[second]=temp;
+    }
 }
