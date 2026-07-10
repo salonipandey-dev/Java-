a381db645436d6f1e721bb6d3cc1ca685bacd6bd
Saloni paney-ux <workspaceworkout0716@gmail.com>
2026-07-10 19:36:55 +0530

    practice

---
+++ JAVAOFFICIAL/DSA/Sorting/FindMissing.java
@@ -0,0 +1,15 @@
+package JAVAOFFICIAL.DSA.Sorting;
+
+public class FindMissing {
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,6};
+        int ans=missingNumber(arr);
+        System.out.println("Missing number is :"+ans);
+    }
+    static int missingNumber(int[] arr)
+    {
+        int i=0;
+        while(i<arr.length)
+        {
+            
+}
