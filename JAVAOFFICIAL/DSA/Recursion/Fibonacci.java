08d43a5a8096183ad8d1ffea73acc0933b4c9229
Saloni paney-ux <workspaceworkout0716@gmail.com>
2026-07-15 19:13:56 +0530

    practice

--- JAVAOFFICIAL/DSA/Recursion/Fibonacci.java
+++ JAVAOFFICIAL/DSA/Recursion/Fibonacci.java
@@ -2,7 +2,7 @@ package JAVAOFFICIAL.DSA.Recursion;
 
 public class Fibonacci {
     public static void main(String[] args) {
-        int n=5;
+        int n=50;
         System.out.println("The fibonacci of "+n+" is: "+fibonacci(n));
     }
     static int fibonacci(int n)
