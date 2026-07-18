9631fcee20f88592c06153bdf6f24cbe3ec0ac87
Saloni paney-ux <workspaceworkout0716@gmail.com>
2026-07-18 17:35:32 +0530

    practice

--- JAVAOFFICIAL/Functions/FindDistintnumber.java
+++ JAVAOFFICIAL/Functions/FindDistintnumber.java
@@ -3,7 +3,9 @@ package JAVAOFFICIAL.Functions;
 public class FindDistintnumber {
     public static void main(String[] args)
     {
-        
+        int[] arr = {2,3,4,1,2,1,3,6,4};
+        System.out.println();
     }
+    public static int 
     
 }
