79717d4789cdd30b1430ae30d1e989041fe3570a
Saloni paney-ux <workspaceworkout0716@gmail.com>
2026-08-18 19:38:57 +0530

    practice

--- JAVAOFFICIAL/DSA/Arrays/Linearsearch/FirstOccurence.java
+++ JAVAOFFICIAL/DSA/Arrays/Linearsearch/FirstOccurence.java
@@ -7,11 +7,17 @@ public class FirstOccurence {
         {
             if(arr[i]==target)
             {
-                return i;
-                break ;
+                System.out.println("Key found at index :" + i);
+                break;
             }
         }
         return -1;
+    }
+    public static void main(String[] args) {
+        int[] arr={4, 7, 2, 7, 9, 7, 1};
+        int target=7;
+        int result=FirstOccur(arr,target);
+        System.out.println("Index for key found at :" + result);
     }
 
 }
