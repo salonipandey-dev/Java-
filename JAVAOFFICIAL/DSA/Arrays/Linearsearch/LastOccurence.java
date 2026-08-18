3250009965e1057d79add8f933b9813a46339dc7
Saloni paney-ux <workspaceworkout0716@gmail.com>
2026-08-18 19:42:15 +0530

    practice

--- JAVAOFFICIAL/DSA/Arrays/Linearsearch/LastOccurence.java
+++ JAVAOFFICIAL/DSA/Arrays/Linearsearch/LastOccurence.java
@@ -1,9 +1,9 @@
 package JAVAOFFICIAL.DSA.Arrays.Linearsearch;
 
-public class FirstOccurence {
-    static int FirstOccur(int[] arr, int target)
+public class LastOccurence {
+    static int LastOccur(int[]arr, int target)
     {
-        for(int i=0;i<arr.length;i++)
+        for(int i=arr.length-1;i>=0;i--)
         {
             if(arr[i]==target)
             {
@@ -12,11 +12,11 @@ public class FirstOccurence {
         }
         return -1;
     }
+    
     public static void main(String[] args) {
         int[] arr={4, 7, 2, 7, 9, 7, 1};
         int target=7;
-        int result=FirstOccur(arr,target);
+        int result=LastOccur(arr,target);
         System.out.println("Index for key found at :" + result);
     }
-
 }
