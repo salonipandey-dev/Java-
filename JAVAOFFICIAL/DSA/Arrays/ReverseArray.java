336ac3c382661de6940ccb58475a07afb41d4f6b
Saloni paney-ux <workspaceworkout0716@gmail.com>
2026-08-26 18:48:52 +0530

    practice

--- JAVAOFFICIAL/DSA/Arrays/ReverseArray.java
+++ JAVAOFFICIAL/DSA/Arrays/ReverseArray.java
@@ -1,1 +1,25 @@
-practice
+class ReverseArray 
+{
+    public static void reverseArray(int[] arr)
+    {
+        int left = 0;
+        int right=arr.length-1;
+        while(left<right)
+        {
+            int temp=arr[left];
+            arr[left]=arr[right];
+            arr[right]=temp;
+            left++;
+            right--;
+        }
+    }
+    public static void main(String[] args)
+    {
+        int[] arr = {4, 8, 15, 16, 23, 42};
+        int result =reverseArray(arr);
+        for(int i = 0; i<arr.length;i++)
+        {
+            System.out.println("Reversed Array " ,+result);
+        }
+    }
+}
